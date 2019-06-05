2085a25100bd6adbbd4840b20c5316587aea43ed
alessandroscarlatti <violanotesnoextras@gmail.com>
2019-06-04 19:55:51 -0500

    connect main to specific hello world component

---
+++ src/TeamEditorComponent/TeamEditor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class TeamEditor extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Team Editor</div>
+        )
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import TeamEditor from './TeamEditorComponent/TeamEditor'
 
 ReactDom.render(
-	<div>Hello World</div>,
+	<TeamEditor />,
 	document.querySelector("#app")
 )
