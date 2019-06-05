9cae5c5419e69c68b5a434fedaeaa56e173a7e05
alessandroscarlatti <violanotesnoextras@gmail.com>
2019-06-04 20:05:22 -0500

    Hello world penguins list

--- src/TeamEditorComponent/TeamEditor.js
+++ src/TeamEditorComponent/TeamEditor.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import ViewingPenguin from './ViewingPenguin';
 
 export default class TeamEditor extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            penguins: [
+                {
+                    id: 0,
+                    state: "VIEW",
+                    data: {
+                        name: "phil"
+                    }
+                },
+                {
+                    id: 1,
+                    state: "VIEW",
+                    data: {
+                        name: "annie"
+                    }
+                }
+            ]
+        }
+
     }
 
     render() {
+
+        let penguins = [];
+
+        this.state.penguins.forEach(penguin => {
+            penguins.push(<ViewingPenguin />)
+        })
+
         return (
-            <div>Team Editor</div>
+            <div>
+                <div>TeamEditor</div>
+                {penguins}
+            </div>
         )
     }
 }
